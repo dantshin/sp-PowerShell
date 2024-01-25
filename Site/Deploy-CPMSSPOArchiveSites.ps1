699e89dd8413ec5a800e971e28b72878a7a50f8f
dantshin <dan@tshin.com>
2024-01-25 20:13:03 +0100

    Refactored code to use function

--- Site/Deploy-CPMSSPOArchiveSites.ps1
+++ Site/Deploy-CPMSSPOArchiveSites.ps1
@@ -25,10 +25,11 @@
 LASTEDIT: $(Get-Date)
 # Written by: Daniel Tshin (daniel.tshin@undp.org) on 24 January 2024
 #
-# version: 1.0
+# version: 1.1
 #
 # Revision history
 # # 24 Jan 2024 Creation
+# # 25 Jan 2024 Refactoring code to use Jobs for concurrency
 
 .FUNCTIONALITY
 		Deploy-CPMSSPOArchiveSites creates a set of SPO archival sites for CPMS sites that are living inside legacy Intranet
@@ -53,11 +54,11 @@ param(
 #$RootsiteUrl = "https://<tenant_name>.sharepoint.com/"
 
 #CSV file structure
-#SiteName,SitePath,LegacySiteURL,SiteURL,SiteOwner,SecondarySiteOwner1,SecondarySiteOwner2
+#SiteName,SitePath,LegacySiteURL,siteUrl,SiteOwner,SecondarySiteOwner1,SecondarySiteOwner2
 #CPMS Archival site for corporate,/sites/corporate,https://intranet/sites/corporate,corporate,siteowner@contoso.com,siteowner2@contoso.com,"Site Owner Group Name"
 #CPMS Archival site for H10,/sites/H10,https://intranet/sites/H10,H10
-$TimeZone = 10 #"UTCMINUS0500_EASTERN_TIME_US_AND_CANADA"
-$SiteTemplateId = "BDR#0" #Document Center
+$timeZone = 10 #"UTCMINUS0500_EASTERN_TIME_US_AND_CANADA"
+$siteTemplateId = "BDR#0" #Document Center
 # Need to trim trailing slashes in $RootsiteUrl
 #Import CSV
 $sites = Import-Csv -Path $InputCSVFile
@@ -65,19 +66,38 @@ $sites = Import-Csv -Path $InputCSVFile
 #Connect to SharePoint Admin Center
 Connect-PnPOnline -Url $AdminUrl -Interactive
 
-$counter = 0
-#Create sites
-foreach ($site in $sites) {
-    try {
-		$siteURL = $RootsiteUrl + "/teams/" + $site.SiteURL + "_archive"
-        New-PnPTenantSite -Template $SiteTemplateId -Title $site.SiteName -Url $siteURL -Owner $site.SiteOwner -TimeZone $TimeZone -Wait
+function NewSPOSite {
+	param(
+		$site, $newSiteUrl, $siteTemplateId, $timeZone
+	)
+	try {
+
+		#Create sites
+		New-PnPTenantSite -Template $siteTemplateId `
+			-Title $site.SiteName `
+			-Url $newSiteUrl `
+			-Owner $site.SiteOwner `
+			-TimeZone $timeZone `
+			-Wait
 		#Add Site Collection admins
-		Set-PnPTenantSite -Identity $siteURL -Owners @($site.SecondarySiteOwner1,$site.SecondarySiteOwner2) -DenyAddAndCustomizePages:$false
-		Write-Host "Created SPO site: $siteURL" -ForegroundColor Green
-    }
+		Set-PnPTenantSite `
+			-Identity $newSiteUrl `
+			-Owners @($site.SecondarySiteOwner1,$site.SecondarySiteOwner2) `
+			-DenyAddAndCustomizePages:$false
+
+		Write-Host "Created SPO site: $newSiteUrl" -ForegroundColor Green
+	}
     catch [System.Net.WebException], [System.IO.IOException] {
-        Write-Host "Unable to create site $($site.SiteURL)" -ForegroundColor Red
+        Write-Host "Unable to create site $newSiteUrl for $($site.siteUrl)" -ForegroundColor Red
     }
+}
+
+$counter = 0
+
+foreach ($site in $sites) {
+	$newSiteUrl = $RootsiteUrl + "/teams/" + $site.siteUrl + "_archive"
+	NewSPOSite -site $site -newSiteUrl $newSiteUrl -siteTemplateId $siteTemplateId -timeZone $timeZone
+
 	$counter++
 	Write-Progress -Activity "Processing SPO Site Creation" -Status "Processed: $counter of $($sites.Count) sites" -PercentComplete (($counter / $sites.Count) * 100)
 }
